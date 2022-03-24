72c17ffc5bfe357df5811129d845115eab64b948
Usuario <alonsocinco@gmail.com>
2022-03-24 01:50:52 +0100

    V3 - Comentarios implementación de DataEmail en CorreoElectronico.java

--- src/Data.java
+++
@@ -1,4 +0,0 @@
-public class Data {
-    private String remitente ="";
-    private String clave="";
-}
---
+++ src/DataEmail.java
@@ -0,0 +1,12 @@
+public class DataEmail {
+    private String remitente ="";
+    private String clave="";
+
+    public String getRemitente() {
+        return remitente;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+}
